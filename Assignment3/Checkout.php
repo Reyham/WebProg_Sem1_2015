961e0637a6119261bdd93637184c9cc9af83affa
PongPow <s3505394@student.rmit.edu.au>
2015-05-29 04:32:12 +1000

    final
    -I can't put the barcode into the table, is really difficult can't find
    anything online.
    -php calcs- logic is there however it there seems to be an error when
    looking at the arrays.

--- Assignment3/Checkout.php
+++ Assignment3/Checkout.php
@@ -14,45 +14,102 @@
 
 <div class="BookingPanel">
 <h1> Success!!</h1>
-<p> Congratulations <? php echo $_POST["name"];?><br>
-<p> An electronic version of your tickets have been sent to <? php $_POST["email"];?><br>
-<p> Your unique login number is<h2 id="unique_no"></h2></p><br>
-<p>The total price for your purchase is <? php echo $price ?><br>
+<p> Congratulations <?php echo $_POST['name'];?><br>
+<p> An electronic version of your tickets have been sent to <?php echo $_POST['email'];?><br>
+<p> Your unique login number is  <h2><?php echo rand(10000,99999)?></h2></p><br>
+
 </div>
-<div class="Tickets">
-<table class="ticketTable"> 
-<thead>
-    <tr>
-<th> Movie Name</th>
-<th> Seat Type</th>
-<th>Time</th></tr>
-</thead>
-<tbody>
+	<?php
+	
+		if(isset($_POST['id'])){
+		$id = $_POST['id'];
+	
+		$_SESSION['cart']['screenings'] = array_values($_SESSION['cart']['screenings']);
+		};
+		if(isset($_SESSION['cart']['screenings'][0])){
+		echo '<div id="ticketsPanel">';
+		foreach($_SESSION['cart']['screenings'] as $id => $ticket){
+			$rows = count($ticket['seats']);
+			echo '<div class="ticketInfo">';
+			echo '<div class="TicketTitle">'.$ticket['movie']."</div>
+			
+				<table border='1px solid black'>
+				<tr><td>Movie</td><td>Ticket Type</td><td>Quantity</td><td>Day</td><td>Time</td></tr>";
+			foreach($ticket['seats'] as $key => $row){
+				echo "<tr><td>".$ticket['movie']."<td>".$key."</td><td>".$row['quantity']."</td><td>".$ticket['day']."</td><td>".$ticket['time']."</td></tr></table>";
+			echo '<img id="barcode" src="barcode.jpg" alt="Barcode >';
+		
+			}
+		echo '<br></div>';
+			
+		};
+				
+		
+			
+	
 
-	<tr>
-<td><strong><? php $_POST['movie']?></strong></td>
-<td> <? php $_POST['Seat_type'] ?></td>
-<td><? php $_POST['day'] ?></td>
-<td> <img id="barcode" src="barcode.jpg" alt="Barcode"></td>
+	
+	
+	
+	
+		echo '</div>';
+		echo '</div>';
+		}
+		?>
+		<?php
+	
+$seat_prices1= array(
+	"SA" => "12",
+    "SP" => "10",
+	"SC" => "8",
+    "FA" => "25",
+	"FC" => "20",
+    "B1" => "20",
+	"B2" => "40",
+    "B3" => "60",
 
-</tbody>
-</table>
-	</div>
+);
+$seat_prices2= array(
+	"SA" => "19",
+    "SP" => "15",
+	"SC" => "12",
+    "FA" => "30",
+	"FC" => "25",
+    "B1" => "30",
+	"B2" => "60",
+    "B3" => "90",
+);
 
-<script>
-function myFunction() {
-    var x = document.getElementById("unique_no")
-    x.innerHTML = Math.floor((Math.random() * 100000) + 1);
-}
-</script>
+//	$total = 0;
+//			foreach($_SESSION['cart']['screenings'] as $id => $ticket){
+//				foreach($ticket['seats'] as $key => $row){
+//					if ($ticket['day']=="Saturady"||$ticket['day'=="Sunday"]||$ticket['time']!="1pm"){
+//			$total= $total+ $seat_prices2[$key] * $row['quantity'];
+ //}
+//	else {
+//		
+//			$total= $total+ $seat_prices1[$key] * $row['quantity'];
+//					}}}
+?>
+<? php if(isset($_POST['sendReservation']))
+{
+    $to      =  $_Post['email'];
+    $subject = 'Movie Tickets';
+    $message = 'Here are your movie tickets'
+
+    $headers = 'From: silverado@cinema.com' . "\r\n" .
+        'Reply-To: Silverado@cinemas.com' . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+
+    mail($to, $subject, $message, $headers);
+
+    echo 'Email Sent.';
+	?>
+
+<div>
+</div>
 
 
-
-
-<?php include 'footer.php';?>
-<form action="" method="post">
-    <input type="submit" value="Send tickets to email" />
-    <input type="hidden" name="button_pressed" value="1" />
-</form>
 </body>
+<?php include 'footer.php';?>
 </html>
